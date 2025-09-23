dbdf348e7438d57b8f388bc7d302d06a05189b86
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-09-23 22:18:31 +0530

    type conversion

---
+++ type_conversion/java.js
@@ -0,0 +1,9 @@
+let age =window.prompt("How old are you?");
+age=Number(age);  //this is converting string to number without this output would be 251 if input is 25 
+age+=1;
+console.log(age);
+
+//for conversion of x to
+// number-use function "Number(x)"
+// string-use function "String(x)"
+// boolean-use function "Boolean(x)" 
