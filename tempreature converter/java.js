207f70fb5d5301e3d09bc49fce48a3eedee8acd5
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-10-18 13:13:52 +0530

    tempreature convertor

--- tempreature converter/java.js
+++ tempreature converter/java.js
@@ -7,11 +7,11 @@ function tempreature(){
     if(tofahrenheit.checked){
         temp=Number(textbox.value);
         temp=temp*(9/5)+32;
-        result.textContent=temp + " F";
+        result.textContent=temp.toFixed(1) + " F";
     }else if(tocelsius.checked){
         temp=Number(textbox.value);
         temp=(5/9)*(temp-32);
-        result.textContent=temp + " C";
+        result.textContent=temp.toFixed(1) + " C";
 
     }else{
         result.textContent="Select a Unit";
