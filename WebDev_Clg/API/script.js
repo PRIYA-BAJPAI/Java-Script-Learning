8aac7b86c4d28960610ffe3b3059aa412fe18239
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2026-05-20 15:58:53 +0530

    styling

--- WebDev_Clg/API/script.js
+++ WebDev_Clg/API/script.js
@@ -4,10 +4,22 @@ fetch("https://dummyjson.com/products").then((res)=>{
     console.log(data.products) 
     let body=document.querySelector("body")
     data.products.map((a)=>{
-        body.innerHTML+=`<div style="border:1px solid black;width:250px;padding:10px;margin:10px">
+        body.innerHTML+=`<div style="border:1px solid black;width:250px;padding:10px;margin:10px;background-color:  rgb(162, 182, 188);border-radius:5px">
         <img src="${a.thumbnail}"width="100%">
         <h2>${a.title}</h2>
         <h3>${a.price}</h3>
+        <button style="background-color: red; color:white;font-size:13px;margin-left:30%; padding: 10px;border:1px solid black ; border-radius:5px ;" onclick="addToCart(${a.id})">Add to Cart </button>
+       
         `
+         window.allP=data.products
     })
 })
+let cart=[]
+function addToCart(id){
+    let data=window.allP.find((a)=>{
+        return a.id==id
+    })
+    cart.push(data)
+    
+    localStorage.setItem("item",JSON.stringify(cart))
+}
